e5b5f487d1376a07372b9d25d4da05bca5f8d6c6
Shu4910 <101110626+Shu4910@users.noreply.github.com>
2023-07-21 15:36:26 +0900

    t

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,10 +4,10 @@
     <title>Page Title</title>
 </head>
 <body>
-
+<!-- 
 <a href="{{ route('user.login') }}">
   <button type="button">個人用ログイン画面</button>
-</a>
+</a> -->
 
 <a href="{{ route('company.login') }}">
   <button type="button">企業用ログイン画面</button>
